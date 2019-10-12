3bb4b60353f7ab547bd1d70486fe124248d6b4dc
Fbrito1 <luan.tecno@gmail.com>
2019-10-12 19:33:14 -0300

    add email schedule initial setup

--- server/src/app/controllers/AlertController.js
+++ server/src/app/controllers/AlertController.js
@@ -1,5 +1,11 @@
+import Queue from 'bull';
 import Alert from '../schemas/Alert';
 import AlertValidator from '../validators/AlertValidator';
+
+const sendMail = new Queue(
+  'sendMail',
+  `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+);
 
 class AlertController {
   async index(req, res) {
@@ -7,12 +13,12 @@ class AlertController {
 
     const alerts = await Alert.find({ user_email: userEmail });
 
-    if(!alerts) {
+    if (!alerts) {
       return res.status(404).json({
         message: 'Cannot found alerts for this email',
         error: true,
       });
-    };
+    }
 
     return res.json(alerts);
   }
@@ -22,11 +28,11 @@ class AlertController {
 
     const schema = await AlertValidator.validateStore(req.body);
 
-    if(schema.isError) {
+    if (schema.isError) {
       return res.status(400).json({
         message: 'Invalid request arguments',
         error: schema.error,
-      })
+      });
     }
 
     const alertAlreadyExists = await Alert.findOne({
@@ -34,12 +40,12 @@ class AlertController {
       search_phrase: searchPhrase,
     });
 
-    if(alertAlreadyExists) {
+    if (alertAlreadyExists) {
       return res.status(400).json({
         message: 'One alert with this search phrase already exists',
         error: schema.error,
-      })
-    };
+      });
+    }
 
     const alert = await Alert.create({
       search_phrase: searchPhrase,
@@ -47,8 +53,15 @@ class AlertController {
       notification_schedule: notificationSchedule,
     });
 
+    const queueData = {
+      notificationSchedule,
+      alert,
+    };
+
+    await sendMail.add(queueData);
+
     return res.status(201).json(alert);
-  };
+  }
 
   async update(req, res) {
     const { alertId } = req.params;
@@ -57,30 +70,31 @@ class AlertController {
 
     const schema = await AlertValidator.validateUpdate(req.body);
 
-    if(schema.isError) {
+    if (schema.isError) {
       return res.status(400).json({
         message: 'Invalid request arguments',
         error: schema.error,
       });
-    };
+    }
 
     const alert = await Alert.findById(alertId);
 
-    if(!alert) {
+    if (!alert) {
       return res.status(404).json({
         message: 'Alert not found',
         error: true,
-      })
+      });
     }
 
-    if(alert.user_email !== userEmail) {
+    if (alert.user_email !== userEmail) {
       return res.status(400).json({
         message: 'Invalid request arguments',
         error: 'You cannot update this alert',
-      })
-    };
+      });
+    }
 
-    const updatedAlert = await Alert.findByIdAndUpdate(alertId,
+    const updatedAlert = await Alert.findByIdAndUpdate(
+      alertId,
       {
         search_phrase: searchPhrase,
         notification_schedule: notificationSchedule,
@@ -89,32 +103,32 @@ class AlertController {
     );
 
     return res.json(updatedAlert);
-  };
+  }
 
-  async delete (req, res) {
+  async delete(req, res) {
     const { alertId } = req.params;
     const { userEmail } = req;
 
     const alert = await Alert.findById(alertId);
 
-    if(!alert) {
+    if (!alert) {
       return res.status(404).json({
         message: 'Alert not found',
         error: true,
-      })
+      });
     }
 
-    if(alert.user_email !== userEmail) {
+    if (alert.user_email !== userEmail) {
       return res.status(400).json({
         message: 'Invalid request arguments',
         error: 'You cannot delete this alert',
-      })
-    };
+      });
+    }
 
     const deletedAlert = await Alert.findByIdAndRemove(alertId);
 
     return res.json(deletedAlert);
-  };
+  }
 }
 
 export default new AlertController();
