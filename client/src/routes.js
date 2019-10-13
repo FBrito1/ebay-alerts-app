b4d378743799d8cb1cb8cb574cdb823f8c2b7d1c
Fbrito1 <luan.tecno@gmail.com>
2019-10-13 09:53:34 -0300

    add new alert design page

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Alert from './pages/Alert';
 import Home from './pages/Home';
+import NewAlert from './pages/NewAlert';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={Home} exact />
-        <Route path="/alerts" component={Alert} />
+        <Route path="/alerts" component={Alert} exact />
+        <Route path="/alerts/new" component={NewAlert} />
       </Switch>
     </BrowserRouter>
   );
